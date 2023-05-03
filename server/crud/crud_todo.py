d8abd51d31cc2813e50d1164d1f725b311b56e29
Vladimir Kostic <vladimir@easyaerial.com>
2023-05-03 16:08:17 +0200

    Add get tasks

--- server/crud/crud_todo.py
+++ server/crud/crud_todo.py
@@ -3,14 +3,14 @@ from server.supabase.supabase_init import supabase
 class CRUDToDo():
     __Table = "ToDo"
 
+    def get_list(self, list_id: int):
+        data = supabase.table(self.__Table).select("*").eq("todo_id", list_id).execute()
+        return data.data[0]
+
     def create_list(self, list_name: str):
         data = supabase.table(self.__Table).insert({"name": list_name}).execute()
         return data.data[0]
     
-    def get_list(self, list_id: int):
-        data = supabase.table(self.__Table).select("todo_id").eq("todo_id", list_id).execute()
-        return data.data[0]
-
     def delete_list(self, list_id: int):
         data = supabase.table(self.__Table).delete().eq("todo_id", list_id).execute()
         return data.data[0]
@@ -18,13 +18,16 @@ class CRUDToDo():
     def modify_list(self, list_name: str):
         pass
 
-    def add_task(self, list_id: int, task_name: dict):
-        data = supabase.table(self.__Table).update({"tasks": task_name}).eq("todo_id", list_id).execute()
+    def get_tasks(self, list_id: int):
+        data = supabase.table(self.__Table).select("tasks").eq("todo_id", list_id).execute()
+        return data.data[0]
+
+    def add_task(self, list_id: int, task_name: str):
+        prev_task_list = self.get_tasks(list_id)
+        new_task_list = { task_name: False}
+        data = supabase.table(self.__Table).update({"tasks": new_task_list}).eq("todo_id", list_id).execute()
         return data.data[0]
     
-    def get_task(self, list_id: int, task_name: str):
-        pass
-
     def delete_task(self, list_name: str, task_name: str):
         pass
 
@@ -33,11 +36,5 @@ class CRUDToDo():
 
     def set_task_status(self, list_name: str, task_name: str, status: bool):
         pass
-    
-
-
-
-    # def read(self):
-    #     data = supabase.table("ToDo").select("*").eq("name", "Germany").execute()
 
 todo = CRUDToDo()
--- server/tests/crud/test_crud_todo.py
+++ server/tests/crud/test_crud_todo.py
@@ -1,31 +1,42 @@
 from server.crud import crud_todo as crud 
 
-todo_list_id: int = 0
+todo_list_id:    int  = 0
 
-def test_todo_create():
+def test_todo_create_list():
     global todo_list_id
-    data = crud.todo.create_list("test-list")
-    todo_list_id = data['todo_id']
+
+    data = crud.todo.create_list('test-list')
+    todo_list_id    = data['todo_id']
 
     assert data['todo_id'] == todo_list_id
-    assert data['name'] == 'test-list'
-    assert data['tasks'] == None
-    print("Test: ToDo list create")
+    assert data['name']    == 'test-list'
+    assert data['tasks']   == None
 
-def test_todo_get():
+def test_todo_get_list():
     data = crud.todo.get_list(todo_list_id)
-    print(data)
+
+    assert data['todo_id'] == todo_list_id
+    assert data['name']    == 'test-list'
+    assert data['tasks']   == None
+
+def test_todo_get_tasks_no_tasks():
+    data = crud.todo.get_tasks(todo_list_id)
+
+    assert data['tasks']   == None 
 
 def test_todo_add_task():
-    data = crud.todo.add_task(todo_list_id, {"test-task1": True})
-    data = crud.todo.add_task(todo_list_id, {"test-task2": False})
+    data = crud.todo.add_task(todo_list_id, 'test-task1')
+    # data = crud.todo.add_task(todo_list_id, {"test-task2": False})
     assert data['todo_id'] == todo_list_id
     assert data['name'] == 'test-list'
     # assert data['tasks'] == {"test-task1": True}
     print(data)
-    print("Test: ToDo list add task")
 
-def test_todo_delete():
+def test_todo_get_tasks_one_task():
+    data = crud.todo.get_tasks(todo_list_id)
+
+    print(data)
+
+def test_todo_delete_list():
     print(todo_list_id)
     assert crud.todo.delete_list(todo_list_id) is not None
-    print("Test: ToDo list delete")
